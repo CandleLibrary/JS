4186ea1646a9b6be17937f44a82619e43623ef51
galactrax <anthonycweathersby@gmail.com>
2019-06-06 07:26:33 -0600

    working on object literal rendering

--- source/nodes/property_binding.mjs
+++ source/nodes/property_binding.mjs
@@ -7,5 +7,5 @@ export default class property_binding extends binding {
         super([sym[0], sym[2]]);
     }
     get type( ){return types.property_binding}
-    render() { return `${this.id.type > 4 ? `[${this.id.render()}]` : this.id.render()} : ${this.init.render()}` }
+    render() { return `${this.id.type > 2 ? `[${this.id.render()}]` : this.id.render()} : ${this.init.render()}` }
 }
