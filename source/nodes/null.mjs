c70244595f9bc37edb71caa743456e33c20126fd
Anthony Weathersby <anthonycweathersby@gmail.com>
2020-02-27 16:18:00 -0700

    removing old js files - preparing to convert to typescript

--- source/nodes/null.mjs
+++
@@ -1,9 +0,0 @@
-/** NULL **/
-
-import base from "./base.mjs";
-import types from "./types.mjs";
-export default class null_literal extends base {
-    constructor() { super() }
-    get type() { return types.null_literal }
-    render() { return "null" }
-}
--- source/nodes/tab_indent.mjs
+++
@@ -1,4 +0,0 @@
-export default function(string, tab_count = 1, indent = "    "){
-	const indent_string = indent.repeat(tab_count);
-	return indent_string + string.replace(/\n/g, "\n" + indent_string);
-}; // 4 Spaces per tab default
