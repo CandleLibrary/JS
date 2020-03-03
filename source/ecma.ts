4b0ff1e83e74a0849400a8798e2170f77ca2e856
galactrax <anthonycweathersby@gmail.com>
2020-03-03 08:12:12 -0700

    Add MinTreeNodeType and MinTreeNodeMaskedType to exports

--- source/ecma.ts
+++ source/ecma.ts
@@ -5,10 +5,13 @@ import { MinTreeNodeDefinition } from "./nodes/min_tree_node_definition.js";
 import parser from "./parser/parser.js";
 
 import { render } from "./tools/render.js";
-
+import { MinTreeNodeMaskedType } from "./nodes/ntype.js";
+import { MinTreeNodeType } from "./nodes/mintree_node_type.js";
 export {
+    MinTreeNodeType,
     MinTreeNodeDefinition,
     MinTreeNodeDefinitions,
+    MinTreeNodeMaskedType,
     MinTreeNode,
     parser,
     render
