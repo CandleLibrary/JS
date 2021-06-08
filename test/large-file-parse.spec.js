16aa3323fea16c7afcb40803031ad11fb9960e49
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-06-08 11:37:32 -0600

    Chore: Modify test

---
+++ test/large-file-parse.spec.js
@@ -0,0 +1,22 @@
+import { parser, parser_new } from "@candlelib/js";
+import URL from "@candlelib/url";
+import { default as s } from "acorn";
+
+const d = await URL.resolveRelative("@candlelib/js/build/library/parser/javascript2.js").fetchText();
+let g = 0;
+
+harness.markTime();
+g = (new s.Parser).parse(d, { sourceType: "module" });
+harness.getTime("Acorn");
+
+harness.markTime();
+g = parser_new(d);
+harness.getTime("Post Hydrocarbon v0.7.* parser");
+
+harness.markTime();
+g = parser(d);
+harness.getTime("Pre Hydrocarbon v0.7.* parser");
+
+g = "";
+
+assert(g == "");
